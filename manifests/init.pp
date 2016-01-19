67c96c98ef6ac173922b5da53cde265c633a6d6e
Neil Parley <mail@neilparley.co.uk>
2016-01-19 14:39:01 +0000

    Pass forward the opal password and password hash to install

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
-class opal ($change_password=false) {
+class opal ($opal_password='password', $opal_password_hash = '$shiro1$SHA-256$500000$dxucP0IgyO99rdL0Ltj1Qg==$qssS60kTC7TqE61/JFrX/OEk0jsZbYXjiGhR7/t+XNY=') {
 
-  class {opal::install: change_password=>$change_password}
+  class {opal::install: opal_password => $opal_password, opal_password_hash => $opal_password_hash}
 
 }
