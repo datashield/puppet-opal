79dcf63d8f51016474a4f683fd5a3bece99b2d8d
Neil Parley <mail@neilparley.co.uk>
2016-01-19 14:37:19 +0000

    Send opal password info to module. Use curl to make sure the web service is up before trying to use any REST services. Login to opal to make sure the file system is made for the admininstrator user.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,6 @@
-class opal::install($change_password=false)  {
+class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SHA-256$500000$dxucP0IgyO99rdL0Ltj1Qg==$qssS60kTC7TqE61/JFrX/OEk0jsZbYXjiGhR7/t+XNY=')  {
+
+  include wait_for
 
   class {'opal::repository': before => [Package['opal'], Package['opal-rserver'], Package['opal-python-client']]}
 
@@ -58,7 +60,15 @@ class opal::install($change_password=false)  {
 
   }
 
-
-  class {opal::update_admin_password: change_password => $change_password}
+  class {opal::update_admin_password: opal_password_hash => $opal_password_hash} ->
+  wait_for { 'curl -s -o /dev/null localhost:8080':
+    exit_code         => 0,
+    polling_frequency => 10.0,
+    max_retries       => 5,
+  } ->
+  exec { 'opal_login' :
+    command => "opal system --opal http://localhost:8080 --user administrator --password '${opal_password}' --conf",
+    path => "/usr/bin:/bin",
+  }
 
 }
