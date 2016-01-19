6898de8877a8d48113d1b2b19db93e6e4e819a0e
Neil Parley <mail@neilparley.co.uk>
2016-01-19 14:33:32 +0000

    Change opal_pass to opal_password to match the other modules. Make the requirement the opal install class and not the opal service.

--- manifests/add_project.pp
+++ manifests/add_project.pp
@@ -1,12 +1,12 @@
-define opal::add_project($opal_path = '/usr/bin/opal', $opal_pass = 'password', $payload="") {
+define opal::add_project($opal_path = '/usr/bin/opal', $opal_password = 'password', $payload="") {
 
   # include ::opal::update_admin_password
 
   exec { "register_db_${name}":
-    command => "/usr/bin/env echo \"${payload}\" | ${opal_path} rest -o http://localhost:8080 -u administrator -p ${opal_pass} -m  POST /projects --content-type \"application/json\"",
-    unless  => "${opal_path} rest /projects --opal http://localhost:8080 --user administrator --password ${opal_pass} --json | grep \"\\\"name\\\": \\\"${name}\\\"\"",
-    require => [Service['opal'], Service['rserver']],
-  } -> Class['::opal::update_admin_password']
+    command => "/usr/bin/env echo \"${payload}\" | ${opal_path} rest -o http://localhost:8080 -u administrator -p '${opal_password}' -m  POST /projects --content-type \"application/json\"",
+    unless  => "${opal_path} rest /projects --opal http://localhost:8080 --user administrator --password '${opal_password}' --json | grep \"\\\"name\\\": \\\"${name}\\\"\"",
+    require => Class[::opal::install],
+  }
 
 }
 
