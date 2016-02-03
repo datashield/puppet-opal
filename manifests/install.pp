bcdb0b598d77f4a5f6d937eef7fab4b381319d9c
Neil Parley <mail@neilparley.co.uk>
2016-02-03 15:01:01 +0000

    Add --fail to curl so it only returns 0 if a 200 status is received

--- manifests/install.pp
+++ manifests/install.pp
@@ -43,7 +43,7 @@ class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SH
   }
 
   class {opal::update_admin_password: opal_password_hash => $opal_password_hash} ->
-  wait_for { 'curl -s -o /dev/null localhost:8080':
+  wait_for { 'curl --fail -s -o /dev/null localhost:8080':
     exit_code         => 0,
     polling_frequency => 15.0,
     max_retries       => 20,
