caebc96e608a959b34a23fdfe084702d4d9683e9
Neil Parley <mail@neilparley.co.uk>
2016-02-10 14:15:13 +0000

    Code reformatting

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@ class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SH
 
   include wait_for
 
-  class {'opal::repository': before => [Package['opal'], Package['opal-rserver'], Package['opal-python-client']]}
+  class { 'opal::repository': before => [Package['opal'], Package['opal-rserver'], Package['opal-python-client']] }
 
   case $::operatingsystem {
     'Ubuntu': {
@@ -19,7 +19,7 @@ class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SH
       package { 'opal-server':
         ensure  => latest,
         require => Class['::r'],
-        alias  => 'opal'
+        alias   => 'opal'
       } ~>
       service { 'opal':
         ensure => running,
@@ -28,21 +28,21 @@ class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SH
     }
   }
 
-  package {'opal-python-client':
-    ensure => latest,
+  package { 'opal-python-client':
+    ensure  => latest,
     require => Package['opal']
   }
 
-  package {'opal-rserver':
-    ensure => latest,
+  package { 'opal-rserver':
+    ensure  => latest,
     require => Package['opal']
   } ~>
   service { 'rserver':
-          ensure => running,
-          enable => true,
+    ensure => running,
+    enable => true,
   }
 
-  class {opal::update_admin_password: opal_password_hash => $opal_password_hash} ->
+  class { opal::update_admin_password: opal_password_hash => $opal_password_hash } ->
   wait_for { 'curl --fail -s -o /dev/null localhost:8080':
     exit_code         => 0,
     polling_frequency => 15.0,
@@ -50,7 +50,7 @@ class opal::install($opal_password='password', $opal_password_hash = '$shiro1$SH
   } ->
   exec { 'opal_login' :
     command => "opal system --opal http://localhost:8080 --user administrator --password '${opal_password}' --conf",
-    path => "/usr/bin:/bin",
+    path    => "/usr/bin:/bin",
   }
 
 }
