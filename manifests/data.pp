a0f2cd12162596153cd8628f1982256a08c9d3fa
Neil Parley <mail@neilparley.co.uk>
2016-02-10 15:18:28 +0000

    Refractor import_data to data

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,11 +1,11 @@
-define opal::import_data($opal_path = '/usr/bin/opal', $opal_password = 'password', $path="") {
+define opal::data($opal_path = '/usr/bin/opal', $opal_password = 'password', $path="") {
 
   # include ::opal::update_admin_password
 
   exec { "import_data_${name}":
     command => "${opal_path} import-xml --opal http://localhost:8080 -u administrator -p '${opal_password}' --path ${path} --destination ${name}",
     unless  => "${opal_path} rest /datasource/${name}/tables --opal http://localhost:8080 --user administrator --password '${opal_password}' --json | grep 'table'",
-    require => Class[::opal::install],
+    require => [Class[::opal::install], Opal::Project[$name]]
   }
 
 }
