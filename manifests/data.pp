9b758666a34d60ca6bb049454dea450021236745
Neil Parley <mail@neilparley.co.uk>
2016-02-15 11:10:56 +0000

    No longer need opal::admin_password here

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,7 +1,5 @@
 define opal::data($opal_path = '/usr/bin/opal', $opal_password = 'password', $opal_url='http://localhost:8080', $path="") {
 
-  # include ::opal::update_admin_password
-
   exec { "import_data_${name}":
     command => "${opal_path} import-xml --opal ${$opal_url} -u administrator -p '${opal_password}' --path ${path} --destination ${name}",
     unless  => "${opal_path} rest /datasource/${name}/tables --opal ${$opal_url} --user administrator --password '${opal_password}' --json | grep 'table'",
