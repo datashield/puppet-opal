c82df3ee6ccf84dba7568947da10b2ae3bcbaaf3
Neil Parley <mail@neilparley.co.uk>
2016-02-15 10:54:30 +0000

    Add stdlib for bool2str

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class opal ($opal_password='password', $opal_password_hash = '$shiro1$SHA-256$500000$dxucP0IgyO99rdL0Ltj1Qg==$qssS60kTC7TqE61/JFrX/OEk0jsZbYXjiGhR7/t+XNY=') {
 
+  include stdlib
   class {opal::install: opal_password => $opal_password, opal_password_hash => $opal_password_hash}
 
 }
